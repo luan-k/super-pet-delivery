5d90799fa54b11dfbada56a5fc043caa4d449d85
Luan Kraisch <luankds@gmail.com>
2023-09-08 13:17:05 -0300

    create user test

--- api/user_test.go
+++ api/user_test.go
@@ -17,6 +17,79 @@ import (
 	"go.uber.org/mock/gomock"
 )
 
+func TestCreateUserAPI(t *testing.T) {
+	user := randomUser()
+	requestUser := createUserRequest{
+		Username: user.Username,
+		FullName: user.FullName,
+		Email:    user.Email,
+	}
+
+	testCases := []struct {
+		name          string
+		requestUser   createUserRequest
+		buildStubs    func(store *mockdb.MockStore)
+		checkResponse func(t *testing.T, recorder *httptest.ResponseRecorder)
+	}{
+		{
+			name:        "OK",
+			requestUser: requestUser,
+			buildStubs: func(store *mockdb.MockStore) {
+				// Define expectations for the CreateUser function in your mock store.
+				store.EXPECT().CreateUser(gomock.Any(), gomock.Any()).Times(1).Return(user, nil)
+			},
+			checkResponse: func(t *testing.T, recorder *httptest.ResponseRecorder) {
+				require.Equal(t, http.StatusOK, recorder.Code)
+				requireBodyMatchUser(t, recorder.Body, user)
+			},
+		},
+		{
+			name:        "BadRequest",
+			requestUser: createUserRequest{}, // Sending an empty request should trigger a bad request.
+			buildStubs: func(store *mockdb.MockStore) {
+				store.EXPECT().CreateUser(gomock.Any(), gomock.Any()).Times(0) // Expect CreateUser not to be called.
+			},
+			checkResponse: func(t *testing.T, recorder *httptest.ResponseRecorder) {
+				require.Equal(t, http.StatusBadRequest, recorder.Code)
+			},
+		},
+		{
+			name:        "InternalServerError",
+			requestUser: requestUser,
+			buildStubs: func(store *mockdb.MockStore) {
+				store.EXPECT().CreateUser(gomock.Any(), gomock.Any()).Times(1).Return(db.User{}, sql.ErrConnDone)
+			},
+			checkResponse: func(t *testing.T, recorder *httptest.ResponseRecorder) {
+				require.Equal(t, http.StatusInternalServerError, recorder.Code)
+			},
+		},
+	}
+
+	for i := range testCases {
+		tc := testCases[i]
+
+		t.Run(tc.name, func(t *testing.T) {
+			ctrl := gomock.NewController(t)
+			defer ctrl.Finish()
+
+			store := mockdb.NewMockStore(ctrl)
+			tc.buildStubs(store)
+
+			server := NewServer(store)
+			recorder := httptest.NewRecorder()
+
+			requestBody, err := json.Marshal(tc.requestUser)
+			require.NoError(t, err)
+
+			request, err := http.NewRequest(http.MethodPost, "/users", bytes.NewReader(requestBody))
+			require.NoError(t, err)
+
+			server.router.ServeHTTP(recorder, request)
+			tc.checkResponse(t, recorder)
+		})
+	}
+}
+
 func TestGetUserAPI(t *testing.T) {
 	user := randomUser()
 
