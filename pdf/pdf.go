37d5f6d7d12a71183825d26e7a98c5f67f8e9283
Luan Kraisch <luankds@gmail.com>
2023-09-19 18:37:59 -0300

    adding client data to pdf fetch

--- pdf/pdf.go
+++ pdf/pdf.go
@@ -17,7 +17,19 @@ type Sale struct {
 	Price       int    `json:"price"`
 	Observation string `json:"observation"`
 	CreatedAt   string `json:"created_at"`
-	// Add more fields as needed
+}
+
+type Client struct {
+	ID                  int64  `json:"id"`
+	FullName            string `json:"full_name"`
+	PhoneWhatsApp       string `json:"phone_whatsapp"`
+	PhoneLine           string `json:"phone_line"`
+	PetName             string `json:"pet_name"`
+	PetBreed            string `json:"pet_breed"`
+	AddressStreet       string `json:"address_street"`
+	AddressNumber       string `json:"address_number"`
+	AddressNeighborhood string `json:"address_neighborhood"`
+	AddressReference    string `json:"address_reference"`
 }
 
 func fetchSaleData(saleID int64) (*Sale, error) {
@@ -49,6 +61,35 @@ func fetchSaleData(saleID int64) (*Sale, error) {
 	return &sale, nil
 }
 
+func fetchClientData(clientID int64) (*Client, error) {
+	// Define the API URL with the client ID
+	apiURL := fmt.Sprintf("http://localhost:8080/clients/%d", clientID)
+
+	// Make an HTTP GET request to fetch client data
+	response, err := http.Get(apiURL)
+	if err != nil {
+		return nil, err
+	}
+	defer response.Body.Close()
+
+	if response.StatusCode != http.StatusOK {
+		return nil, fmt.Errorf("API request failed with status code: %d", response.StatusCode)
+	}
+
+	// Parse the JSON response
+	var client Client
+	data, err := io.ReadAll(response.Body)
+	if err != nil {
+		return nil, err
+	}
+
+	if err := json.Unmarshal(data, &client); err != nil {
+		return nil, err
+	}
+
+	return &client, nil
+}
+
 func main() {
 	// Define the sale ID you want to retrieve
 	saleID := int64(33)
@@ -57,6 +98,13 @@ func main() {
 	sale, err := fetchSaleData(saleID)
 	if err != nil {
 		fmt.Println("Error fetching sale data from API:", err)
+		return
+	}
+
+	// Fetch client data using the client_id from the sale response
+	client, err := fetchClientData(sale.ClientID)
+	if err != nil {
+		fmt.Println("Error fetching client data from API:", err)
 		return
 	}
 
@@ -70,11 +118,23 @@ func main() {
     <body>
         <img style="width: 333px;" src="img.png" />
         <h1>Sale Details</h1>
-        <p>ID: {{.ID}}</p>
-        <p>Product: {{.Product}}</p>
-        <p>Price: ${{.Price}}</p>
-        <p>Observation: {{.Observation}}</p>
-        <p>Created At: {{.CreatedAt}}</p>
+        <p>ID: {{.Sale.ID}}</p>
+        <p>Product: {{.Sale.Product}}</p>
+        <p>Price: ${{.Sale.Price}}</p>
+        <p>Observation: {{.Sale.Observation}}</p>
+        <p>Created At: {{.Sale.CreatedAt}}</p>
+        
+        <h1>Client Details</h1>
+        <p>ID: {{.Client.ID}}</p>
+        <p>Full Name: {{.Client.FullName}}</p>
+        <p>Phone (WhatsApp): {{.Client.PhoneWhatsApp}}</p>
+        <p>Phone (Line): {{.Client.PhoneLine}}</p>
+        <p>Pet Name: {{.Client.PetName}}</p>
+        <p>Pet Breed: {{.Client.PetBreed}}</p>
+        <p>Address Street: {{.Client.AddressStreet}}</p>
+        <p>Address Number: {{.Client.AddressNumber}}</p>
+        <p>Address Neighborhood: {{.Client.AddressNeighborhood}}</p>
+        <p>Address Reference: {{.Client.AddressReference}}</p>
     </body>
     </html>
     `
@@ -90,14 +150,22 @@ func main() {
 	}
 	defer file.Close()
 
-	// Execute the template with the sale data and write the result to the file
+	// Execute the template with the sale and client data and write the result to the file
 	tmpl, err := template.New("html").Parse(htmlTemplate)
 	if err != nil {
 		fmt.Println("Error parsing HTML template:", err)
 		return
 	}
 
-	err = tmpl.Execute(file, sale)
+	data := struct {
+		Sale   *Sale
+		Client *Client
+	}{
+		Sale:   sale,
+		Client: client,
+	}
+
+	err = tmpl.Execute(file, data)
 	if err != nil {
 		fmt.Println("Error executing template:", err)
 		return
@@ -107,12 +175,18 @@ func main() {
 
 	// Use Gotenberg to convert the HTML to PDF, including the image parameter
 	pdfFilePath := "test.pdf"
+	// Run the Gotenberg command
 	gotenbergCmd := exec.Command("curl", "--request", "POST", "http://localhost:3000/forms/chromium/convert/html", "--form", fmt.Sprintf("files=@\"%s\"", filePath), "--form", "files=@\"img.png\"", "-o", pdfFilePath)
 
-	// Run the Gotenberg command and wait for it to finish
-	err = gotenbergCmd.Run()
-	if err != nil {
-		fmt.Println("Error converting HTML to PDF using Gotenberg:", err)
+	// Start the command
+	if err := gotenbergCmd.Start(); err != nil {
+		fmt.Println("Error starting Gotenberg command:", err)
+		return
+	}
+
+	// Wait for the command to finish
+	if err := gotenbergCmd.Wait(); err != nil {
+		fmt.Println("Error waiting for Gotenberg command to finish:", err)
 		return
 	}
 
