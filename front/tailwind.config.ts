396bc57e96d55a3fcc04a0a2d65948109e3d2651
Luan Kraisch <luankds@gmail.com>
2023-11-01 08:53:30 -0300

    setting up front

--- front/tailwind.config.ts
+++ front/tailwind.config.ts
@@ -1,20 +1,45 @@
-import type { Config } from 'tailwindcss'
+import type { Config } from "tailwindcss";
 
 const config: Config = {
   content: [
-    './pages/**/*.{js,ts,jsx,tsx,mdx}',
-    './components/**/*.{js,ts,jsx,tsx,mdx}',
-    './app/**/*.{js,ts,jsx,tsx,mdx}',
+    "./pages/**/*.{js,ts,jsx,tsx,mdx}",
+    "./components/**/*.{js,ts,jsx,tsx,mdx}",
+    "./app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
     extend: {
       backgroundImage: {
-        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
-        'gradient-conic':
-          'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
+        "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
+        "gradient-conic":
+          "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
+      },
+      colors: {
+        ls: {
+          blue: {
+            dark: "#0C0910",
+            light: "#151526",
+          },
+        },
+      },
+      fontFamily: {
+        Inconsolata: ["Inconsolata", "monospace"],
+        SourceSansPro: ["Source Code Pro", "monospace"],
+      },
+      screens: {
+        "2xl": "1685px",
+      },
+    },
+    container: {
+      center: true,
+      padding: {
+        DEFAULT: "1rem",
+        sm: "2rem",
+        lg: "7%",
+        xl: "8%",
+        "2xl": "10%",
       },
     },
   },
   plugins: [],
-}
-export default config
+};
+export default config;
