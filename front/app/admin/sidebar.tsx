26568d5e074e69921c6c00a831205d59d7ee0770
Luan Kraisch <luankds@gmail.com>
2024-01-21 15:19:44 -0300

    adding sidebar icons and initial stylying

--- front/app/admin/sidebar.tsx
+++ front/app/admin/sidebar.tsx
@@ -5,6 +5,13 @@ import { useEffect, useState } from "react";
 import { FaUser } from "react-icons/fa";
 import { MdOutlinePointOfSale } from "react-icons/md";
 import "../styles/admin/sidebar.scss";
+// now to import the svg for home, vendas, produtos, clientes galeria e usuarios
+import HomeIcon from "../../public/admin-home.svg";
+import VendasIcon from "../../public/admin-vendas.svg";
+import ProdutosIcon from "../../public/admin-produtos.svg";
+import ClientesIcon from "../../public/admin-clientes.svg";
+import GaleriaIcon from "../../public/admin-galeria.svg";
+import UsuariosIcon from "../../public/admin-usuarios.svg";
 
 export default function AdminSidebar() {
   const router = useRouter();
@@ -20,19 +27,61 @@ export default function AdminSidebar() {
 
   const isActive = (href: string) => {
     // Compare the current route with the link's href
-    return currentRoute === href ? "active" : "";
+    return currentRoute === href
+      ? "wkode-admin-sidebar__menu-item--active"
+      : "";
   };
 
   return (
-    <div className='h-full w-1/12 wkode-admin-sidebar '>
-      <Link className={isActive("/admin/clientes")} href={"/admin/clientes"}>
-        <FaUser />
-        Clientes
-      </Link>
-      <Link className={isActive("/admin/vendas")} href={"/admin/vendas"}>
-        <MdOutlinePointOfSale />
-        Vendas
-      </Link>
+    <div className='wkode-admin-sidebar '>
+      <div className='wkode-admin-sidebar__menu-wrapper'>
+        <Link
+          className={`wkode-admin-sidebar__menu-item ${isActive("/admin")}`}
+          href={"/admin"}>
+          <HomeIcon />
+          {/* Home */}
+        </Link>
+        <Link
+          className={`wkode-admin-sidebar__menu-item ${isActive(
+            "/admin/clientes"
+          )}`}
+          href={"/admin/clientes"}>
+          <ClientesIcon />
+          {/*  Clientes */}
+        </Link>
+        <Link
+          className={`wkode-admin-sidebar__menu-item ${isActive(
+            "/admin/produtos"
+          )}`}
+          href={"/admin/produtos"}>
+          <ProdutosIcon />
+          {/* Produtos */}
+        </Link>
+        <Link
+          className={`wkode-admin-sidebar__menu-item ${isActive(
+            "/admin/vendas"
+          )}`}
+          href={"/admin/vendas"}>
+          <VendasIcon />
+          {/* Vendas */}
+        </Link>
+        <Link
+          className={`wkode-admin-sidebar__menu-item ${isActive(
+            "/admin/galeria"
+          )}`}
+          href={"/admin/galeria"}>
+          <GaleriaIcon />
+          {/* Galeria */}
+        </Link>
+        <Link
+          className={`wkode-admin-sidebar__menu-item ${isActive(
+            "/admin/usuarios"
+          )}`}
+          href={"/admin/usuarios"}>
+          <UsuariosIcon />
+          {/* Usuarios */}
+        </Link>
+      </div>
     </div>
   );
 }
