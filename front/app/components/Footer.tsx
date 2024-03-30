d674fe0ab5882e8fa5fa4948d762dd86a2522f48
Luan Kraisch <luankds@gmail.com>
2024-03-30 02:36:38 -0300

    footer links

--- front/app/components/Footer.tsx
+++ front/app/components/Footer.tsx
@@ -4,84 +4,104 @@ import Image from "next/image";
 import Logo from "../../public/static/images/superpet-logo.png";
 import FooterAnimals from "../../public/static/images/animaisfootermini.png";
 import WKode from "../../public/wkode-footer-logo.svg";
-import { FaWhatsapp, FaInstagram, FaMapMarker } from 'react-icons/fa';
+import { FaWhatsapp, FaInstagram, FaMapMarker } from "react-icons/fa";
 import { usePathname } from "next/navigation";
 import { useEffect, useState } from "react";
 
 export default function Footer() {
+  const [currentRoute, setCurrentRoute] = useState("");
+  const pathname = usePathname();
 
-    const [currentRoute, setCurrentRoute] = useState("");
-    const pathname = usePathname();
+  useEffect(() => {
+    // Update the current route when the route changes
+    setCurrentRoute(pathname);
+  }, [pathname]);
 
-    useEffect(() => {
-        // Update the current route when the route changes
-        setCurrentRoute(pathname);
-      }, [pathname]);
- 
-    return(
-        <footer className={`wk-footer ${
+  return (
+    <footer
+      className={`wk-footer ${
         pathname === "/login" || pathname.startsWith("/admin") ? "hidden" : ""
-        }`}>
-            <Image src={FooterAnimals} alt="" className="wk-footer-image" />
-            <div className="container grid grid-cols-1 lg:grid-cols-4 p-20">
-                <div className="wk-footer-section">
-                    <Image draggable={false} src={Logo} alt="Superpet" />
-                    <div className="wk-footer-text">
-                        Sabemos que os animais de estimação são como uma família, por isso estamos comprometidos em fornecer produtos da mais alta qualidade em que você pode confiar.
-                    </div>
-                    <div className="wk-footer-icons">
-                        <Link href="https://www.google.com/maps/place/Super+Pet+Delivery/@-27.6316157,-48.6558265,15z/data=!4m6!3m5!1s0x95273597c288c591:0x428507ff8e06abda!8m2!3d-27.6316157!4d-48.6558265!16s%2Fg%2F11tx4q5rtv?entry=ttu">
-                            <FaMapMarker />
-                        </Link>
-                        <Link href="https://api.whatsapp.com/send?phone=554899805164&text=Ol%C3%A1%2C%20gostaria%20de%20mais%20informa%C3%A7%C3%B5es%20sobre%20os%20produtos%20da%20SuperPet.">
-                            <FaWhatsapp />
-                        </Link>
-                        <Link href="https://www.instagram.com/superpetdelivery/">
-                            <FaInstagram />
-                        </Link>
-                    </div>
-                </div>
-                <div className="wk-footer-section">
-                    <h3>Institucional</h3>
-                    <Link href='/'>Início</Link>
-                    <Link href='/produtos'>Produtos</Link>
-                    <Link href='/sobre-nos'>Sobre nós</Link>
-                    <Link href='/contato'>Contato</Link>
-                </div>
-                <div className="wk-footer-section">
-                    <h3>Categorias</h3>
-                    <Link href='#'>Cachorros</Link>
-                    <Link href='#'>Gatos</Link>
-                    <Link href='#'>Medicamentos</Link>
-                    <Link href='#'>Acessorios</Link>
-                    <Link href='#'>Ofertas</Link>
-                </div>
-                <div className="wk-footer-section">
-                    <h3>Fale Conosco</h3>
-                    <div className="wk-form">
-                        <label>
-                            <h4>Nome</h4>
-                            <input type="text" name="name" required placeholder='Seu nome' />
-                        </label>
-                        <label>
-                            <h4>Email</h4>
-                            <input type="email" name="email" required placeholder='Seu email' />
-                        </label>
-                        <label>
-                            <h4>Telefone</h4>
-                            <input type="phone" name="phone" required placeholder='Seu telefone' />
-                        </label>
-                        <label>
-                            <h4>Mensagem</h4>
-                            <textarea name="message" cols={30} rows={10} placeholder='Sua mensagem' ></textarea>
-                        </label>
-                        <button className='wk-btn wk-btn--sm wk-btn--primary' type='submit'>Enviar</button>
-                    </div>
-                </div>
-            </div>
-            <div className="wk-footer-main container p-20 flex items-center justify-end">
-                <WKode />
-            </div>
-        </footer>
-    )
+      }`}>
+      <Image src={FooterAnimals} alt='' className='wk-footer-image' />
+      <div className='container grid grid-cols-1 lg:grid-cols-4 p-20'>
+        <div className='wk-footer-section'>
+          <Image draggable={false} src={Logo} alt='Superpet' />
+          <div className='wk-footer-text'>
+            Sabemos que os animais de estimação são como uma família, por isso
+            estamos comprometidos em fornecer produtos da mais alta qualidade em
+            que você pode confiar.
+          </div>
+          <div className='wk-footer-icons'>
+            <Link href='https://www.google.com/maps/place/Super+Pet+Delivery/@-27.6316157,-48.6558265,15z/data=!4m6!3m5!1s0x95273597c288c591:0x428507ff8e06abda!8m2!3d-27.6316157!4d-48.6558265!16s%2Fg%2F11tx4q5rtv?entry=ttu'>
+              <FaMapMarker />
+            </Link>
+            <Link href='https://api.whatsapp.com/send?phone=554899805164&text=Ol%C3%A1%2C%20gostaria%20de%20mais%20informa%C3%A7%C3%B5es%20sobre%20os%20produtos%20da%20SuperPet.'>
+              <FaWhatsapp />
+            </Link>
+            <Link href='https://www.instagram.com/superpetdelivery/'>
+              <FaInstagram />
+            </Link>
+          </div>
+        </div>
+        <div className='wk-footer-section'>
+          <h3>Institucional</h3>
+          <Link href='/'>Início</Link>
+          <Link href='/produtos'>Produtos</Link>
+          <Link href='/sobre-nos'>Sobre nós</Link>
+          <Link href='/contato'>Contato</Link>
+        </div>
+        <div className='wk-footer-section'>
+          <h3>Categorias</h3>
+          <Link href='/produtos?category=Cachorros'>Cachorros</Link>
+          <Link href='/produtos?category=Gatos'>Gatos</Link>
+          <Link href='/produtos?category=Medicamentos'>Medicamentos</Link>
+          <Link href='/produtos?category=Acessórios-e-Brinquedos'>
+            Acessórios e Brinquedos
+          </Link>
+          <Link href='/produtos?category=Promoções'>Promoções</Link>
+        </div>
+        <div className='wk-footer-section'>
+          <h3>Fale Conosco</h3>
+          <div className='wk-form'>
+            <label>
+              <h4>Nome</h4>
+              <input type='text' name='name' required placeholder='Seu nome' />
+            </label>
+            <label>
+              <h4>Email</h4>
+              <input
+                type='email'
+                name='email'
+                required
+                placeholder='Seu email'
+              />
+            </label>
+            <label>
+              <h4>Telefone</h4>
+              <input
+                type='phone'
+                name='phone'
+                required
+                placeholder='Seu telefone'
+              />
+            </label>
+            <label>
+              <h4>Mensagem</h4>
+              <textarea
+                name='message'
+                cols={30}
+                rows={10}
+                placeholder='Sua mensagem'></textarea>
+            </label>
+            <button className='wk-btn wk-btn--sm wk-btn--primary' type='submit'>
+              Enviar
+            </button>
+          </div>
+        </div>
+      </div>
+      <div className='wk-footer-main container p-20 flex items-center justify-end'>
+        <WKode />
+      </div>
+    </footer>
+  );
 }
